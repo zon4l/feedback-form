cf45c315f2ef5e2e3a6748c31d3b7bdcab8e0bf3
Arbaaz Khan <arbaaz.afraaz@gmail.com>
2022-05-11 09:35:31 +0530

    added feedback form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Card from "./shared/Card";
+import RatingSelect from "./RatingSelect";
 
 import { useState } from "react";
 import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   function handleTextChange(e) {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   }
 
@@ -15,6 +30,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             type="text"
@@ -22,10 +38,12 @@ function FeedbackForm() {
             onChange={handleTextChange}
             value={text}
           />
-          <Button type="submit" version="secondary">
+          <Button type="submit" version="secondary" isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
